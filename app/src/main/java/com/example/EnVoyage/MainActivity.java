7a3c77f748bd90c3bbcfdfd67cb7faa3cc50f1e3
Yasmine <yt2003@my.yorku.ca>
2023-02-19 03:15:57 -0500

    Commit

--- app/src/main/java/com/example/EnVoyage/MainActivity.java
+++ app/src/main/java/com/example/EnVoyage/MainActivity.java
@@ -5,11 +5,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
+        @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
